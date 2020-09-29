2cfb5a36f024f936676da2313fe0e7f56f7e2472
Marina Nicolai <nicolaimarina@gmail.com>
2020-09-29 09:33:21 -0400

    adding main section on the page

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import "../App.css";
 import Nav from "./Nav";
 import Footer from "./Footer";
+import Main from "./Main";
 
 function App() {
   return (
     <div className="App">
       <Nav />
-
+      <Main />
       <Footer />
     </div>
   );
---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Main() {
+  return (
+    <>
+      <h1>This is the main section</h1>
+    </>
+  );
+}
+
+export default Main;
