ccf063848241f1d62161a94fcbd4b3eeaac432c9
Marina Nicolai <nicolaimarina@gmail.com>
2020-10-13 11:35:53 -0400

    footer update

--- src/components/theme/Footer/index.jsx
+++ src/components/theme/Footer/index.jsx
@@ -1,20 +1,34 @@
 import React from "react";
+import { Flex, Details, Links } from "./styles";
+import social from "./social.json";
 
 function Footer() {
   return (
-    <>
-      <div className="site-copyright">
-        <span className="site-copyright-year">2020 </span>
-        <span className="site-copyrigt-name">Marina Nicolai</span>
-      </div>
-      <nav className="footer">
-        <ul className="menu">
-          <li>Timeline</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
-    </>
+    <Flex>
+      <Details>
+        <span>
+          © All rights are reserved | {new Date().getFullYear()} | Made with{" "}
+          <span aria-label="love" role="img">
+            💖
+          </span>{" "}
+          by Marina Nicolai
+        </span>
+      </Details>
+      <Links>
+        {social.map(({ id, name, link, icon }) => (
+          <a
+            key={id}
+            href={link}
+            target="_blank"
+            rel="noopener"
+            rel="noopener noreferrer"
+            aria-label={`follow me on ${name}`}
+          >
+            <img width="24" src={icon} alt={name} />
+          </a>
+        ))}
+      </Links>
+    </Flex>
   );
 }
 
---
+++ src/components/theme/Footer/styles.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const Flex = styled.div`
+  display: flex;
+  align-items: flex-end;
+  justify-content: space-between;
+`;
+
+export const Details = styled.div`
+  h2,
+  a,
+  span {
+    color: #212121;
+  }
+`;
+
+export const Links = styled.div`
+  display: flex;
+  align-items: center;
+  a {
+    margin: 0 0.5rem;
+    img {
+      margin: 0;
+    }
+    &:first-child,
+    &:last-child {
+      margin: 0;
+    }
+  }
+`;
