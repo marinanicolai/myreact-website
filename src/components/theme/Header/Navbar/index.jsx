0c170604328bba9b91092b295e10603daccb5b7d
Marina Nicolai <nicolaimarina@gmail.com>
2020-10-13 12:20:32 -0400

    header updates

--- src/components/theme/Header/Navbar/index.jsx
+++ src/components/theme/Header/Navbar/index.jsx
@@ -1,13 +1,28 @@
 import React, { useContext } from "react";
 import { Link } from "react";
 import NavbarLinks from "../NavbarLinks";
-import { Wrapper, Brand } from "./styles";
+import { Wrapper, Links } from "./styles";
+import social from "./social.json";
 
 const Navbar = () => {
   return (
     <Wrapper>
       <div as={Link} to="/">
-        Marina Nicolai
+        Marina Nicolai{" "}
+        <Links>
+          {social.map(({ id, name, link, icon }) => (
+            <a
+              key={id}
+              href={link}
+              target="_blank"
+              rel="noopener"
+              rel="noopener noreferrer"
+              aria-label={`follow me on ${name}`}
+            >
+              <img width="24" src={icon} alt={name} />
+            </a>
+          ))}
+        </Links>
       </div>
       <NavbarLinks desktop />
     </Wrapper>
--- src/components/theme/Header/Navbar/styles.js
+++ src/components/theme/Header/Navbar/styles.js
@@ -6,3 +6,19 @@ export const Wrapper = styled.div`
   align-items: center;
   justify-content: space-between;
 `;
+
+export const Links = styled.span`
+  display: flex;
+  align-items: center;
+  a {
+    padding: 2rem;
+    margin: 0 0.5rem;
+    img {
+      margin: 0;
+    }
+    &:first-child,
+    &:last-child {
+      margin: 0;
+    }
+  }
+`;
