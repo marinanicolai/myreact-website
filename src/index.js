64a6685e235cbfb7f305dda42ea2e218bf355d9b
Marina Nicolai <nicolaimarina@gmail.com>
2020-10-13 12:34:47 -0400

    file structure update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./components/theme/index.js";
+//import App from "./components/theme/index.js";
+import App from "./pages/index.js";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Nav from "../components/theme/Header/Navbar";
+import Footer from "../components/theme/Footer/index";
+import Main from "../components/theme/Main/Main";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Main />
+      <Footer />
+      test
+    </div>
+  );
+}
+
+export default App;
